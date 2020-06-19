4b8b54ae9c24beb20fbb116d4cac6ad9385857e2
Philip Pfeffer <philip@pfeffer.org>
2020-06-19 18:25:43 -0300

    Adding my project's model link and classes

--- app/server.py
+++ app/server.py
@@ -9,10 +9,10 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
-export_file_name = 'export.pkl'
+export_file_url = 'https://drive.google.com/uc?export=download&id=1r2BpsDxPdI4-_2TwX2-Gi99XiNOcIaSa'
+export_file_name = 'clothes_export.pkl'
 
-classes = ['black', 'grizzly', 'teddys']
+classes = ['jeans','trousers','jacket', 'shorts', 'tshirt', 'buttonup']
 path = Path(__file__).parent
 
 app = Starlette()
